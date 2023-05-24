2e7632c87f3edcf475f2bc2737a33d09eb01b950
BrianElmo <105883668+Brianelmo@users.noreply.github.com>
2023-05-24 13:22:42 -0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({name}) {
   return (
     <div>
-      <a download={'VitaeBrianElmo'} href="/src/assets/Curriculum-BrianElmo.pdf"
+      <a download={'CurriculumBrianElmo'} href="/src/assets/Curriculum-BrianElmo.pdf"
         className="text-white py-4 px-7
            bg-blue-400 rounded-full text-white-500 font-bold hover:bg-blue-500 shadow-2xl transition-all delay-75">{name}</a> 
            
