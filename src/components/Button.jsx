3c897540502f0944f06fa0057e6b0b9eff3d61d5
Brian Elmo <brianelmo14@gmail.com>
2023-12-29 13:03:50 -0300

    cambio url button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({name}) {
   return (
     <div>
-      <a download='CurriculumBrianElmo' href="/src/assets/cv.pdf"
+      <a download='CurriculumBrianElmo' href="../assets/cv.pdf"
         className="text-white py-4 px-7
            bg-blue-400 rounded-full text-white-500 font-bold hover:bg-blue-500 shadow-2xl transition-all delay-75">{name}</a> 
     </div>
